0e6048b2577f71cf7886a7418ddbf1f890e0205a
Egor <evn88fx64@gmail.com>
2020-12-23 23:45:39 +0400

    refactoring

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
 class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
+  state = { date: new Date() };
 
   componentDidMount() {
     this.timerID = setInterval(() => this.tick(), 1000);
