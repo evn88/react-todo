e4445a085c9adb957732932f94abe84a3d4b033b
Egor <evn88fx64@gmail.com>
2020-12-23 23:48:51 +0400

    add action click

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -2,11 +2,35 @@ import "./InputForm.scss";
 import React from "react";
 
 class InputForm extends React.Component {
+  state = {
+    inputText: "",
+  };
+
+  handleInputTodo = ({ target: { value } }) => {
+    this.setState({
+      inputText: value,
+    });
+  };
+
+  handleAddTodo = (e) => {
+    e.preventDefault();
+    this.setState({ inputText: "" });
+    console.log(this.state.inputText);
+  };
+
   render() {
+    const { inputText } = this.state;
     return (
       <form className="inputForm">
-        <input type="text" value="test" />
-        <button type="submit">⏎</button>
+        <input
+          type="text"
+          name="todoInput"
+          placeholder="Enter item"
+          value={inputText}
+          onChange={this.handleInputTodo}
+          autoFocus
+        />
+        <button onClick={this.handleAddTodo}>⏎</button>
       </form>
     );
   }
